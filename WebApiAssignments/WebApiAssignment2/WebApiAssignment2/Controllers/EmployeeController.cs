74f81695f8718420ebfc12aa7a98e1f6748fbb40
MPavaniSudha <98110996+MPavaniSudha@users.noreply.github.com>
2022-05-30 14:16:16 +0530

    MVC_Assignment

---
+++ WebApiAssignments/WebApiAssignment2/WebApiAssignment2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiAssignment2.Models;
+
+namespace WebApiAssignment2.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        ZensarDBEntities zdb = new ZensarDBEntities();
+        public IHttpActionResult GetEmployeeSalById(int eid)
+        {
+            var details = zdb.getEmployeeSalary(eid).ToList();
+            return Ok(details);
+        }
+    }
+}
