0beec1f310f0cf4687f7b904739590acb3c5ac93
MPavaniSudha <98110996+MPavaniSudha@users.noreply.github.com>
2022-05-30 14:18:55 +0530

    MVC

---
+++ MVC_Assgnments/MVC_Assignment1/MVC_Assignment1/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Assignment1.Models;
+
+namespace MVC_Assignment1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NorthwindEntities nw = new NorthwindEntities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //1.Getting all customers residing in germany
+        public ActionResult Get_Cust_Res_Germany()
+        {
+            List<string> Cnam = (from cus in nw.Customers
+                                 where cus.Country == "Germany"
+                                 select cus.ContactName).ToList();
+            return View(Cnam);
+        }
+        //2.Getting Customer details whose customer id is 10248
+        public ActionResult Get_Customer_Details()
+        {
+            List<Customer> CUST = (from cus in nw.Customers
+                                   join or in nw.Orders
+                                   on cus.CustomerID equals or.CustomerID
+                                   where or.OrderID == 10248
+                                   select cus).ToList();
+            return View(CUST);
+        }
+    }
+}
---
+++ MVC_Assgnments/MVC_Assignment2/MVC_Assignment2/Controllers/MovieTableController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Assignment2.Models;
+
+namespace MVC_Assignment2.Controllers
+{
+    public class MovieTableController : Controller
+    {
+        MoviedbEntities mb = new MoviedbEntities();
+        // GET: MovieTable
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //1. Getting the Movies details from scaffolded view
+        public ActionResult GetMoviesScaffolded()
+        {
+            List<MovieTable> mvt = mb.MovieTables.ToList();
+            return View(mvt);
+        }
+        //2.Crud Operations to create and post of create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(MovieTable mt)
+        {
+            mb.MovieTables.Add(mt);
+            mb.SaveChanges();
+            return RedirectToAction("GetMoviesScaffolded");
+        }
+        //3.get details of a particular movie
+        public ActionResult Details(int id)
+        {
+            MovieTable mt = mb.MovieTables.Find(id);
+            return View(mt);
+        }
+        //4.Edit records
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            MovieTable ms = mb.MovieTables.Find(id);
+            return View(ms);
+        }
+        [HttpPost]
+        public ActionResult Edit(MovieTable mt)
+        {
+            MovieTable m = mb.MovieTables.Find(mt.mid);
+            m.moviename= mt.moviename;
+            m.dateofrelease = mt.dateofrelease;
+            mb.SaveChanges();
+            return RedirectToAction("GetMoviesScaffolded");
+        }
+        //5.Delete a record
+        public ActionResult Delete(int id)
+        {
+            MovieTable m = mb.MovieTables.Find(id);
+            mb.MovieTables.Remove(m);
+            mb.SaveChanges();
+            return RedirectToAction("GetMoviesScaffolded");
+        }
+    }
+}
